2a754411fd851e364ec393241e1d112a5adc135c
McSohan <mcsohan6300@gmail.com>
2024-11-30 15:30:50 -0800

    remove deb

--- application/forge.config.js
+++ application/forge.config.js
@@ -19,13 +19,6 @@ module.exports = {
       platforms: ['darwin'],
     },
     {
-      name: '@electron-forge/maker-deb',
-      config: {
-        authors: 'Sohan',
-        description: 'Eisenhower matrix',
-      },
-    },
-    {
       name: '@electron-forge/maker-rpm',
       config: {
         authors: 'Sohan',
