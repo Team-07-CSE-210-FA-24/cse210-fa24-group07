d0d90c079af6260cd4aa110599cc49adab4fb83d
Yashraj19 <gyashraj1919@gmail.com>
2024-12-10 02:10:02 -0800

    Added in the functionality to make the storage persistent, wherein all the added tasks are locally stored even after closing the app. It is done using electron store which saved the data in a json file.

--- application/preload.js
+++ application/preload.js
@@ -6,5 +6,5 @@ contextBridge.exposeInMainWorld('electronAPI', {
   addTask: (task) => ipcRenderer.invoke('add-task', task),
   getTasks: () => ipcRenderer.invoke('get-tasks'),
   deleteTask: (quadrant, index) =>
-    ipcRenderer.invoke('delete-task', { quadrant, index }), // New method
+    ipcRenderer.invoke('delete-task', { quadrant, index }),
 });
